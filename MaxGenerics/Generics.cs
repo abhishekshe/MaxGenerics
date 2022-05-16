1e3eed5b0a59682e5c6e8c4d965d057ff4558be3
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-16 12:27:28 +0530

    adding Generics

---
+++ MaxGenerics/Generics.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+
+        T[] Value;
+
+        public GenericMaximum(T[] Value)
+        {
+            this.Value = Value;
+        }
+
+        public T[] Sort(T[] Value)
+        {
+            Array.Sort(Value);
+            return Value;
+        }
+
+        public T MaxValue(params T[] Value)
+        {
+            var sorted_value = Sort(Value);
+            return sorted_value[^1];
+        }
+
+        public T MaxMethod()
+        {
+            var  max = MaxValue(this.Value);
+            return max;
+        }
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.Value);
+            Console.WriteLine("Max value is" + max);
+        }
+    }
+}
--- MaxGenerics/Program.cs
+++ MaxGenerics/Program.cs
@@ -6,20 +6,25 @@ namespace GenericsDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("find max problem");
+            int[] arr = { 112, 22, 222, 432, 555, 678 };
 
-            int maxInt = NonGenericClass.MaxIntegerNumber(23, 24, 22);
-            Console.WriteLine("max integer value is\t" + maxInt);
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.PrintMaxValue();
 
-            Console.WriteLine("find max problem");
+            //Console.WriteLine("find max problem");
 
-            double maxDouble = NonGenericClass.MaxDoubleNumber(23.4, 24.7, 22.1);
-            Console.WriteLine("max integer value is\t" + maxDouble);
+            //int maxInt = NonGenericClass.MaxIntegerNumber(23, 24, 22);
+            //Console.WriteLine("max integer value is\t" + maxInt);
 
-            Console.WriteLine("find max problem");
+            //Console.WriteLine("find max problem");
 
-            string maxString = NonGenericClass.MaxString("Apple", "Peach", "Banana");
-            Console.WriteLine("max integer value is\t" + maxString);
+            //double maxDouble = NonGenericClass.MaxDoubleNumber(23.4, 24.7, 22.1);
+            //Console.WriteLine("max integer value is\t" + maxDouble);
+
+            //Console.WriteLine("find max problem");
+
+            //string maxString = NonGenericClass.MaxString("Apple", "Peach", "Banana");
+            //Console.WriteLine("max integer value is\t" + maxString);
 
 
         }
